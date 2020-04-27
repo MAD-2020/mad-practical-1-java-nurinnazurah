2a1558342c9379cb3df87cc90fb04165b27b7591
nurinnazurah <56821408+nurinnazurah@users.noreply.github.com>
2020-04-27 18:33:26 +0800

    update

--- Question3.java
+++ Question3.java
@@ -11,6 +11,11 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
+    
+    int number = in.nextIn();
+    int result = result = number * number;
+    System.out.printIn("Result : " + result);
     
   }
 }
